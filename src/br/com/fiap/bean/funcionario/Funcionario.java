55b05c104bdbe262ac0c8d10eea6d53cca8ce704
Enzoreal100 <enzoreal100@gmail.com>
2025-06-10 11:14:30 -0300

    feat: added code for saving workers on an array

--- src/br/com/fiap/bean/funcionario/Funcionario.java
+++ src/br/com/fiap/bean/funcionario/Funcionario.java
@@ -3,52 +3,28 @@ package br.com.fiap.bean.funcionario;
 import java.util.UUID;
 
 public class Funcionario {
-    private UUID id;
+    private final UUID id = UUID.randomUUID();
     private String nome;
     private String registro;
 
-    public Funcionario(
-            String nome,
-            String registro
+    protected Funcionario(
     ) {
-        id = UUID.randomUUID();
-        nome = this.nome;
-        registro = this.registro;
     }
 
-
-    public UUID getId() {
+    protected UUID getId() {
         return id;
     }
-    public String getNome() {
+    protected String getNome() {
         return nome;
     }
-    public void setNome(String nome) {
+    protected void setNome(String nome) {
         this.nome = nome;
     }
-    public String getRegistro() {
+    protected String getRegistro() {
         return registro;
     }
-    public void setRegistro(String registro) {
+    protected void setRegistro(String registro) {
         this.registro = registro;
     }
 
-    public Funcionario registrar(String nome, String registro) {
-        try{
-            if (nome != null && nome.length() > 2){
-                if (registro != null && registro.length() > 2){
-                    Funcionario funcionario = new Funcionario(nome, registro);
-                    System.out.println(funcionario.getNome());
-                    System.out.printf("Usuário %s criado com sucesso!", funcionario.getNome());
-                    return funcionario;
-                } else {
-                    throw new Error("O registro deve ter mais de 2 caracteres");
-                }
-            } else {
-                throw new Error("O nome deve ter mais de 2 caracteres");
-            }
-        }catch (Exception err) {
-            throw new Error(err);
-        }
-    };
 }
