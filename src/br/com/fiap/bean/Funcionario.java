2af36b07014cd7f068c6035f568a7afeff749819
Enzoreal100 <enzoreal100@gmail.com>
2025-06-13 12:21:39 -0300

    Restarting project

--- src/br/com/fiap/bean/Funcionario.java
+++ src/br/com/fiap/bean/Funcionario.java
@@ -1,4 +1,30 @@
 package br.com.fiap.bean;
 
 public class Funcionario {
+    private String idFuncionario;
+    private String nome;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String idFuncionario, String nome) {
+        this.idFuncionario = idFuncionario;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(String idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
 }
--- src/br/com/fiap/bean/Retirada.java
+++ src/br/com/fiap/bean/Retirada.java
@@ -1,4 +1,67 @@
 package br.com.fiap.bean;
 
+import java.time.LocalDate;
+
 public class Retirada {
+    private String idRetirada;
+    private Insumo insumoRetirado;
+    private int quantidade;
+    private LocalDate dataRetirada;
+    private Funcionario responsavelRetirada;
+
+    public Retirada() {
+    }
+
+    public Retirada(String idRetirada, Insumo insumoRetirado, int quantidade, LocalDate dataRetirada, Funcionario responsavelRetirada) {
+        this.idRetirada = idRetirada;
+        this.insumoRetirado = insumoRetirado;
+        this.quantidade = quantidade;
+        this.dataRetirada = dataRetirada;
+        this.responsavelRetirada = responsavelRetirada;
+    }
+
+    public String getIdRetirada() {
+        return idRetirada;
+    }
+
+    public void setIdRetirada(String idRetirada) {
+        this.idRetirada = idRetirada;
+    }
+
+    public Insumo getInsumoRetirado() {
+        return insumoRetirado;
+    }
+
+    public void setInsumoRetirado(Insumo insumoRetirado) {
+        this.insumoRetirado = insumoRetirado;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(LocalDate dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public Funcionario getResponsavelRetirada() {
+        return responsavelRetirada;
+    }
+
+    public void setResponsavelRetirada(Funcionario responsavelRetirada) {
+        this.responsavelRetirada = responsavelRetirada;
+    }
+
+    public void registrarRetirada(){
+
+    }
+    public void exibirComprovante(){}
 }
